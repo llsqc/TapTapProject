a64781bce3cc5f917820f866d1fa8ce05dfbe1b3
isolation <23796978@qq.com>
2024-10-08 18:08:30 +0800

    新建Project

---
+++ Source/TapTapProject/TapTapProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TapTapProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TapTapProject, "TapTapProject" );
